4bb4071a3372ad138a5ae2783ec1dad55f282955
windhamdavid <david@davidawindham.com>
2016-02-02 18:51:47 -0500

    2 players

---
+++ src/js/radio.js
@@ -0,0 +1,22 @@
+/**** Audio Player ****/
+
+Amplitude.init({
+  "songs": [
+    {
+  		"url": "http://64.207.176.250:8080/iwoody",
+  		"live": true,
+  	},
+    {
+      "url": "http://64.207.152.217:8000/stream",
+      "live": true,
+  	}
+  ],
+    "autoplay": true,
+    "amplitude_volume": 100
+});
+
+/**** Page Features ****/
+
+$(document).ready(function() {
+
+});

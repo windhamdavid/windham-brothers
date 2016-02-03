44338791ef0d8f75acce5d872cde9786044a834f
windhamdavid <david@davidawindham.com>
2016-02-02 20:18:59 -0500

    wb

--- src/js/radio.js
+++ src/js/radio.js
@@ -3,12 +3,12 @@
 Amplitude.init({
   "songs": [
     {
-  		"url": "http://64.207.176.250:8080/iwoody",
-  		"live": true,
-  	},
-    {
       "url": "http://64.207.152.217:8000/stream",
       "live": true,
+  	},
+    {
+  		"url": "http://64.207.176.250:8080/iwoody",
+  		"live": true,
   	}
   ],
     "autoplay": true,
